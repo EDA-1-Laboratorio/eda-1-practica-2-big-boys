4b85329e4261bcf6197c476ac2b52127a42824b2
silvaeliu221 <silva.eliu221@gmail.com>
2026-02-19 10:36:45 -0600

    Respuesta
    Añadimos la explicación si se usara otra condición

--- ejercicio_1.c
+++ ejercicio_1.c
@@ -36,3 +36,9 @@ int main() {
 //Durante el ciclo de intercambio (swap), la condición de parada es while (inicio < fin). 
 //Explica detalladamente qué es lo que se está comparando físicamente en esa instrucción (¿valores o direcciones?) 
 // y qué sucedería si la condición fuera while (*inicio != *fin).
+// RESPUESTA: Aquí se comparan direcciones de la memoria; el ciclo continúa siempre y cuando el apuntador inicio esté antes 
+// que fin en la memoria, y se detiene cuando se cruzan; viéndolo como un ejemplo práctico, es como si una persona alcanzara 
+// a otra exactamente en el centro.
+// Si se usa la otra condicion, se compararían los valores (letras), por lo que si las letras fueran las mismas en los extremos,
+// dejaría nuestra cadena mal invertida. Por ejemplo, en "aroma", como empieza y termina con 'a', el ciclo se detendría 
+// inmediatamente y la cadena no se invertiría correctamente.
